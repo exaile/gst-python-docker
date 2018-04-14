dea010589a96f168ae650f61c4361dfa095c502d
Christian Stadelmann <dev@genodeftest.de>
2018-04-14 22:04:16 +0200

    Fedora: Prepare for Fedora 28

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 cd `dirname $0`
 
-TOBUILD=${1:-debian8 debian9 fedora26 fedora27 ubuntu16.04 ubuntu17.10}
+TOBUILD=${1:-debian8 debian9 fedora27 fedora28 ubuntu16.04 ubuntu17.10}
 BASE=docker.io/exaile/gst-python
 
 for i in ${TOBUILD}; do
