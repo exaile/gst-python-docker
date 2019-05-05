9b739a112af6e5bdb68cef97ab8b7d62fed2f893
Christian Stadelmann <dev@genodeftest.de>
2019-05-05 12:31:05 +0200

    Add docker images with python 3

--- build.sh
+++ build.sh
@@ -2,7 +2,9 @@
 
 cd `dirname $0`
 
-TOBUILD=${1:-debian9 debian10 fedora31 ubuntu18.04 ubuntu19.10}
+TOBUILD=${1:-debian9 debian10 fedora31 ubuntu18.04 ubuntu19.10 \
+	debian9-python3 debian10-python3 fedora31-python3 \
+	ubuntu18.04-python3 ubuntu19.10-python3}
 BASE=docker.io/exaile/gst-python
 
 for i in ${TOBUILD}; do
